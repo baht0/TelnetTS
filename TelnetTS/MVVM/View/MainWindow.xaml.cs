719f179a6b288d4e8454bdf2f00e0461f55449f4
Rasim <bahtiarov.rasim@gmail.com>
2023-10-11 11:23:11 +0300

    removed window change

--- TelnetTS/MVVM/View/MainWindow.xaml.cs
+++ TelnetTS/MVVM/View/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -17,7 +16,6 @@ namespace TelnetTS.MVVM.View
         {
             InitializeComponent();
             DataContext = ViewModel;
-            Application.Current.Activated += CurrentOnActivated;
         }
         private void Window_Closing(object sender, CancelEventArgs e)
         {
@@ -29,32 +27,9 @@ namespace TelnetTS.MVVM.View
             if ((e.Key == Key.Space || e.Key == Key.Enter) && MainButton.IsEnabled)
                 ViewModel.RequestCommand.Execute(ViewModel.Allowed);
         }
-        private bool IsWindowOpen<T>(string name = "") where T : Window
-        {
-            return string.IsNullOrEmpty(name)
-               ? Application.Current.Windows.OfType<T>().Any()
-               : Application.Current.Windows.OfType<T>().Any(w => w.Name.Equals(name));
-        }
-        private void CurrentOnActivated(object sender, EventArgs eventArgs)
-        {
-            if (IsWindowOpen<Window>("consoleWindow"))
-            {
-                var win = Application.Current.Windows.OfType<Window>().Where(w => w.Name.Equals("consoleWindow")).FirstOrDefault();
-                win.WindowState = WindowState.Normal;
-            }
-            this.Activate();
-        }
-        private void Window_StateChanged(object sender, EventArgs e)
-        {
-            var window = (Window)sender;
-            if (window.WindowState == WindowState.Minimized && IsWindowOpen<Window>("consoleWindow"))
-            {
-                var win = Application.Current.Windows.OfType<Window>().Where(w => w.Name.Equals("consoleWindow")).FirstOrDefault();
-                win.WindowState = WindowState.Minimized;
-            }
-        }
 
         private void dataCopy_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => Clipboard.SetText(((TextBlock)sender)?.Text);
+
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             if (e.Uri.OriginalString != "-")
@@ -68,29 +43,17 @@ namespace TelnetTS.MVVM.View
                 e.Handled = true;
             }
         }
-
         private void ConsoleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!IsWindowOpen<Window>("consoleWindow"))
-            {
-                var win = new ConsoleWindow(ViewModel);
-                win.Show();
-            }
-            else
-            {
-                var win = Application.Current.Windows.OfType<Window>().Where(w => w.Name.Equals("consoleWindow")).FirstOrDefault();
-                win.Show();
-                win.WindowState = WindowState.Normal;
-                win.Activate();
-            }
+            var win = new ConsoleWindow(ViewModel);
+            win.ShowDialog();
         }
 
         private void ConnectPointIP_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Middle && e.ButtonState == MouseButtonState.Pressed && ViewModel.OpenCommand.CanExecute(ViewModel.ConnectPoint))
-                ViewModel.OpenCommand.Execute(null);
+            if (e.ChangedButton == MouseButton.Middle && e.ButtonState == MouseButtonState.Pressed && ViewModel.ConnectPoint.OpenCommand.CanExecute(ViewModel.ConnectPoint))
+                ViewModel.ConnectPoint.OpenCommand.Execute(null);
         }
-
         private void Setting_Click(object sender, RoutedEventArgs e)
         {
             var win = new SettingWindow();
